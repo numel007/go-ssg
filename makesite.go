675f0a01b4469a0ac2ac00b0bbd59c28fa314001
Jackie <taekwondo.chan@gmail.com>
2022-01-26 21:24:20 -0800

    did things not according to class instructions

--- makesite.go
+++ makesite.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"os"
+	"text/template"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Bullshit struct {
+	Data string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	data, _ := ioutil.ReadFile("./first-post.txt")
+
+	myStruct := Bullshit{Data: string(data)}
+
+	// Use a defined template
+	parsedTemplate, _ := template.ParseFiles("template.html")
+
+	// Create a file to write to
+	newFile, _ := os.Create("first-post.html")
+
+	// Write to new file using template and data
+	err := parsedTemplate.Execute(newFile, myStruct)
+	check(err)
 }
