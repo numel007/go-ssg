1ad51b6ae9b386a5204a9fba9f49d9284bf486ee
Jackie <taekwondo.chan@gmail.com>
2022-01-31 19:06:55 -0800

    add flags to define input post name

--- makesite.go
+++ makesite.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"os"
+	"strings"
 	"text/template"
 )
 
@@ -17,15 +19,19 @@ type Bullshit struct {
 }
 
 func main() {
-	data, _ := ioutil.ReadFile("./first-post.txt")
+	inputFile := flag.String("file", "first-post.txt", "txt file to pass in")
+	flag.Parse()
+
+	outputName := strings.Split(*inputFile, ".")[0] + ".html"
+	data, _ := ioutil.ReadFile(*inputFile)
 
 	myStruct := Bullshit{Data: string(data)}
 
 	// Use a defined template
-	parsedTemplate, _ := template.ParseFiles("template.html")
+	parsedTemplate, _ := template.ParseFiles("template.tmpl")
 
 	// Create a file to write to
-	newFile, _ := os.Create("first-post.html")
+	newFile, _ := os.Create(outputName)
 
 	// Write to new file using template and data
 	err := parsedTemplate.Execute(newFile, myStruct)
