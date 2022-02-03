4b6d50f9e4fbd87c7ec2fbc58b15db96d214f754
Jackie <taekwondo.chan@gmail.com>
2022-02-02 23:08:08 -0800

    convert all txt files to html on -dir

--- makesite.go
+++ makesite.go
@@ -18,22 +18,46 @@ type Bullshit struct {
 	Data string
 }
 
+// type Page struct {
+// 	TextFilePath string
+// 	TextFileName string
+// 	HTMLFilePath string
+// 	Content      string
+// }
+
 func main() {
-	inputFile := flag.String("file", "first-post.txt", "txt file to pass in")
+	// inputFile := flag.String("file", "first-post.txt", "txt file to pass in")
+	inputDir := flag.String("dir", " ", "Return all .txts in directory")
 	flag.Parse()
 
-	outputName := strings.Split(*inputFile, ".")[0] + ".html"
-	data, _ := ioutil.ReadFile(*inputFile)
+	if *inputDir != " " {
+		println("Converting all txt files in " + *inputDir + "/")
+		files, fileError := ioutil.ReadDir(*inputDir)
+		check(fileError)
 
-	myStruct := Bullshit{Data: string(data)}
+		for _, file := range files {
+			if file.IsDir() {
+			} else {
+				fileNameArray := strings.Split(file.Name(), ".")
+				if fileNameArray[len(fileNameArray)-1] == "txt" {
+					outputName := strings.Split(file.Name(), ".")[0] + ".html"
+					data, _ := ioutil.ReadFile(file.Name())
 
-	// Use a defined template
-	parsedTemplate, _ := template.ParseFiles("template.tmpl")
+					myStruct := Bullshit{Data: string(data)}
 
-	// Create a file to write to
-	newFile, _ := os.Create(outputName)
+					// Use a defined template
+					parsedTemplate, _ := template.ParseFiles("template.tmpl")
 
-	// Write to new file using template and data
-	err := parsedTemplate.Execute(newFile, myStruct)
-	check(err)
+					// Create a file to write to
+					newFile, _ := os.Create(outputName)
+
+					// Write to new file using template and data
+					err := parsedTemplate.Execute(newFile, myStruct)
+					check(err)
+				}
+			}
+		}
+	} else {
+		println("-dir not passed. No htmls generated.")
+	}
 }
